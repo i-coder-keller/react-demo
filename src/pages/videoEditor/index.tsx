780611b0432973cb46a99bad26def069c7acf89c
刘永志 <50040262+i-coder-keller@users.noreply.github.com>
2022-09-01 23:04:35 +0800

    解析富文本

--- src/pages/videoEditor/index.tsx
+++ src/pages/videoEditor/index.tsx
@@ -1,10 +1,28 @@
 import React, {useRef} from "react"
+import { aggregation } from './module/aggregation'
 import './index.less'
 export default () => {
   const editor: any = useRef()
   const changeEditor = (e: any) => {
-    const parse = new DOMParser()
-    console.log(editor.current.childNodes)
+    const nodes = aggregation(editor.current.childNodes).map(elNode => {
+      if (Array.isArray(elNode.data)) {
+        return elNode.data.map(el => {
+          const element = document.createElement(elNode.type) as HTMLImageElement
+          element.src = el
+          return element
+        })
+      }
+      const element = document.createElement(elNode.type) as HTMLDivElement
+      if (typeof elNode.data === "string") {
+        element.innerText = elNode.data
+      }
+      return element
+    })
+    editor.current.innerHTML = ''
+    console.log(nodes)
+    nodes.flat().forEach(element => {
+      editor.current.appendChild(element)
+    })
   }
   return (
     <div className="editor-container" ref={editor} contentEditable="true" onInput={changeEditor}>
---
+++ src/pages/videoEditor/module/aggregation.ts
@@ -0,0 +1,23 @@
+const imgReg = /<img.*?(?:>|\/>)/gi
+const srcRef = /src=['"]?([^'"]*)['"]?/i;
+export const aggregation = (nodes: HTMLElement[]): Array<{type: string; data: Array<string> | string}> => {
+  const result = []
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].outerHTML.includes('img')) {
+      const container = document.createElement('div')
+      container.innerHTML = nodes[i].outerHTML
+      const src = [].map.call(container.querySelectorAll('img'), (img: HTMLImageElement) => img.src)
+      result.push({
+        type: 'img',
+        data: src
+      })
+    }
+    if (nodes[i].outerText !== '') {
+      result.push({
+        type: 'p',
+        data: nodes[i].outerText
+      })
+    }
+  }
+  return result
+}
